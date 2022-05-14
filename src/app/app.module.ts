186599d71c9a458c0f8e3ad1cbcc7ad62559d6a4
IgnacioMercado <ignaciomercado00@gmail.com>
2022-05-14 20:41:19 -0300

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';  
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { ArticulosFamiliasComponent } from './articulos-familias/articulos-familias.component';
-import { HttpClientModule } from "@angular/common/http";
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent, InicioComponent, ArticulosFamiliasComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [ 
+    BrowserModule, 
+    FormsModule, 
+    HttpClientModule, 
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+      { path: 'inicio', component: InicioComponent },
+      { path: 'articulosfamilias', component: ArticulosFamiliasComponent }
+    ])
+],
+  declarations: [ 
+    AppComponent, 
+    HelloComponent, 
+    InicioComponent, 
+    ArticulosFamiliasComponent 
+    ],
+  bootstrap: [ 
+    AppComponent 
+    ]
 })
 export class AppModule { }
