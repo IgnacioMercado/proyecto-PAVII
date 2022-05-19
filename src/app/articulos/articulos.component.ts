6f02de19d7b90edda076f4691e516ec93e3c3a5c
IgnacioMercado <ignaciomercado00@gmail.com>
2022-05-19 11:02:38 -0300

    2 files modified, 1 added

--- src/app/articulos/articulos.component.ts
+++ src/app/articulos/articulos.component.ts
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./articulos.component.css']
 })
 export class ArticulosComponent implements OnInit {
+  Titulo = 'Articulos'
+  TituloAccionABMC = {
+    A: "(Agregar)",
+    B: "(Eliminar)",
+    M: "(Modificar)",
+    C: "(Consultar)",
+    L: "(Listado)"
+  };
+
 
   constructor() { }
 
